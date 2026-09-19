a5dd8a16f32c016d22a55498ef5bc70d0d4d0de5
samhitag3 <114625736+samhitag3@users.noreply.github.com>
2026-09-19 01:45:48 -0700

    Delete modular-perception directory

--- modular-perception/tests/test_masks.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-
-def test_instance_to_union_and_binary():
-    m = np.array([[0,1,1],[2,0,2]], dtype=np.uint16)
-    assert np.array_equal(m > 0, np.array([[False,True,True],[True,False,True]]))
-    assert (m == 1).sum() == 2
-    assert (m == 2).sum() == 2
